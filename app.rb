c386ab2c8aaa2b9204139d8f5630425ad7f3a6b0
Ben Cornelis & Leon Zagorouiko <student@epicodus.com>
2015-04-27 16:13:01 -0700

    Make it pretty.

--- app.rb
+++ app.rb
@@ -13,9 +13,13 @@ get('/output') do
   height = params.fetch('height').to_i
   weight = params.fetch('weight').to_i
   distance = params.fetch('distance').to_i
-  gift_wrap = params.fetch('gift_wrap').==('on')
 
-  parcel = Parcel.new(height, length, width, weight, gift_wrap)
+  if params.has_key?('gift_wrap')
+    parcel = Parcel.new(height, length, width, weight, true)
+  else
+    parcel = Parcel.new(height, length, width, weight, false)
+  end
+  
   @user_entered = ![length, width, height, weight, distance].include?(0)
   @cost = parcel.cost_to_ship(distance)
   erb(:output)
