15cd74a3a43de5325335fd2bc56d65ca707ccdce
Ben Cornelis & Leon Zagorouiko <student@epicodus.com>
2015-04-27 13:54:06 -0700

    Add cost_to_ship method based on distance, volume, and weight.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,7 +1,10 @@
+require('pry')
 class Parcel
 
   cost_per_mile = 2
   cost_per_pound = 3
+  oversize_volume = 64
+  oversize_fee = 30
 
   define_method(:initialize) do |height, length, width, weight|
     @height, @length, @width, @weight = height, length, width, weight
@@ -12,6 +15,10 @@ class Parcel
   end
 
   define_method(:cost_to_ship) do |distance|
-    
+    cost = distance * cost_per_mile + @weight * cost_per_pound
+    if volume() >= oversize_volume
+       cost = cost + oversize_fee
+    end
+    cost
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -9,4 +9,15 @@ describe(Parcel) do
     end
   end
 
+  describe('#cost_to_ship') do
+    it('returns the cost to ship based off distance and weight') do
+      test_parcel = Parcel.new(2,3,4,10)
+      expect(test_parcel.cost_to_ship(50)).to(eq(130))
+    end
+
+    it('adds on a fee if the parcel is oversized') do
+      test_parcel = Parcel.new(4,4,4,10)
+      expect(test_parcel.cost_to_ship(50)).to(eq(160))
+    end
+  end
 end
